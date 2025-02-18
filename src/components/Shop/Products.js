a412b08d8dbf93b86e8d8fb698a3b6ea8199ac73
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-18 17:07:12 +0300

    feat: add to cart handler

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -48,6 +48,7 @@ const Products = (props) => {
         {productList.map((product) => (
           <ProductItem
             key={product.id}
+            id={product.id}
             title={product.title}
             price={product.price}
             description={product.description}
