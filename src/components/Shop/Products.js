ec1fb5d0ac47c4694ff5cc07229287c657a062b6
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-18 16:11:06 +0300

    chore: add dummy data

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -7,9 +7,34 @@ const Products = (props) => {
       <h2>Buy your favorite products</h2>
       <ul>
         <ProductItem
-          title='Test'
+          title='Test 1'
           price={6}
           description='This is a first product - amazing!'
+        />
+        <ProductItem
+          title='Test 2'
+          price={5}
+          description='This is a the second product - amazing!'
+        />
+        <ProductItem
+          title='Test 3'
+          price={7}
+          description='This is a the third product - amazing!'
+        />
+        <ProductItem
+          title='Test 4'
+          price={9}
+          description='This is a fourth product - amazing!'
+        />
+        <ProductItem
+          title='Test 5'
+          price={4}
+          description='This is a fifth product - amazing!'
+        />
+        <ProductItem
+          title='Test 6'
+          price={8}
+          description='This is a sixth product - amazing!'
         />
       </ul>
     </section>
