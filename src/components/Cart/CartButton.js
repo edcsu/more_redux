b6d793a878d24157b4aac1364806fa1447bd1b94
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-18 17:10:16 +0300

    style: show total quantity

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,9 +1,10 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './CartButton.module.css';
 import { uiSliceActions } from '../../store/uiSlice';
 
 const CartButton = (props) => {
   const dispatch = useDispatch()
+  const totalQuantity = useSelector((state) => state.cart.totalQuantity)
   const toggleHandler = () => {
     dispatch(uiSliceActions.toggle())
   }
@@ -11,7 +12,7 @@ const CartButton = (props) => {
   return (
     <button className={classes.button} onClick={toggleHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{totalQuantity}</span>
     </button>
   );
 };
