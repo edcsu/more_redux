37206e420b29941bd58c75d1675b6bdd3bc14faf
Ssewannonda Keith Edwin <skeith696@gmail.com>
2025-02-18 16:51:06 +0300

    refactor: add remove cart reducer function

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -18,6 +18,19 @@ const cartSlice = createSlice({
                     totalPrice: newItem.price,
                     name: newItem.title
                 })
+            } else {
+                existingItem.quantity++
+                existingItem.totalPrice = existingItem.totalPrice + newItem.price
+            }
+        },
+        removeItemFromCart(state, action) {
+            const id = action.payload
+            const existingItem = state.items.find(item => item.id === id)
+            if (existingItem.quantity === 1) {
+                state.items =  state.items.filter(item => item.id !== id)
+            } else {
+                existingItem.quantity--
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price
             }
         }
     }
